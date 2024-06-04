fd46d77a8ab28ea228bd4eb25d269f1e78b72368
Jael <Jael@DESKTOP-P9SV4FQ>
2024-06-04 16:49:10 -0300

    agrgado de atributos a las clases

--- tp4_SilvaJael/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_SilvaJael/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,5 +1,54 @@
 package ar.edu.unju.fi.model;
 
 public class Carrera {
+	private String codigo;
+	private String nombre;
+	private Integer cantidadAnios;
+	private Boolean estado;
+	
+	 public Carrera() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Carrera(String codigo, String nombre, Integer cantidadAnios, Boolean estado) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantidadAnios = cantidadAnios;
+		this.estado = estado;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCantidadAnios() {
+		return cantidadAnios;
+	}
+
+	public void setCantidadAnios(Integer cantidadAnios) {
+		this.cantidadAnios = cantidadAnios;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	 
+	 
 }
